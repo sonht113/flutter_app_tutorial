21ca3b0d7c12abca708270d09cfe3351367c4ef5
Hồ Trọng Sơn <nuiit@MBP-cua-Ho.lan>
2022-08-29 21:32:26 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+	  return MaterialApp(
+		  debugShowCheckedModeBanner: false,
+		  theme: ThemeData(
+			  primarySwatch: Colors.green,
+		  ),
+		  home: const RootPage(),
+	  );
+  }
+}
+
+class RootPage extends StatefulWidget {
+	const RootPage({Key? key}) : super(key: key);
+
+	@override
+	State<RootPage> createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+	@override
+	Widget build(BuildContext context) {
+		return Scaffold(
+			appBar: AppBar(
+				title: const Text('Flutter'),
+			),
+			floatingActionButton: FloatingActionButton(
+				onPressed: () {
+					debugPrint('abc');
+				},
+				child: const Icon(Icons.add),
+			),
+			bottomNavigationBar: NavigationBar(
+				destinations: const [
+					NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+					NavigationDestination(icon: Icon(Icons.person), label: 'Profile')
+				],
+			)
+		);
+	}
+}
