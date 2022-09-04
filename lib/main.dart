5c1953b1925e08173d3dd039107a8c4a622c167b
Hồ Trọng Sơn <nuiit@MBP-cua-Ho.lan>
2022-09-04 12:53:21 +0700

    update code 04 09

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:first_app/home_page.dart';
+import 'package:first_app/profile_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -29,13 +30,18 @@ class RootPage extends StatefulWidget {
 
 class _RootPageState extends State<RootPage> {
   int currentPage = 0;
+  List<Widget> pages = const [
+	  HomePage(),
+	  ProfilePage()
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: const Text('My App'),
         ),
-        body: const HomePage(),
+        body: pages[currentPage],
         floatingActionButton: FloatingActionButton(
           onPressed: () {
             debugPrint('abc');
